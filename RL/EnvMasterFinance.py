79401f34b2a23a4093b9d9c6358d4cde3a40ca82
Ugo LAZIAMOND <ulaziamond@gmail.com>
2024-07-27 14:07:48 +0200

    Re arrange the code

--- RL/EnvMasterFinance.py
+++ RL/EnvMasterFinance.py
@@ -1,124 +1,110 @@
-from Tools import Order
 import numpy as np
-from math import log
 import random as rd
+from Tools import Order
 
 class TradingEnvIAR(Order):
-    def __init__(self, data, window_size=20,episode_size=250,n=1):
+    def __init__(self, data: np.array, window_size: int = 20, episode_size: int = 250, n: int = 1, initial_step: int = -1):
         self.n = n
         self.episode_size = episode_size
         self.window_size = window_size
         self.data = data
         self.action_size = 3
-        #self.max_steps = np.shape(data)[0] - 1
-
-        self.initial_step = rd.randint(self.window_size,len(self.data)-(self.n*self.episode_size+1))
-        self.state_size = (self.window_size,np.shape(data[:,1:])[1])
-        self.current_step = self.initial_step
-        self.position = 0
-        self.historic_position = np.zeros((self.window_size,1))
-        self.done = False
-        self.orders = []
-
-        self.historic_action = np.full((self.window_size,1),0)
-        self.wallet = 0
-
-
-    def reset(self,initial_step:int=-1):
-        if initial_step <0:
-           self.initial_step = rd.randint(self.window_size,len(self.data)-(self.n*self.episode_size+1))
-        else:
-           self.initial_step=initial_step
-        self.current_step = self.initial_step
-        self.done = False
-
-        self.wallet = 0
-        self.orders = []
-        self.position = 0
         
-        self.historic_position = np.zeros((self.window_size,1))
-        self.historic_action = np.full((self.window_size,1),0)
-        self.historic_wallet = np.full((self.window_size,1),self.wallet)
+        self.initial_step = rd.randint(self.window_size, len(self.data) - (self.n * self.episode_size + 1)) if initial_step < 0 else initial_step
+        
+        self.state_size = (self.window_size, np.shape(data[:, 1:])[1])
+        self.current_step = self.initial_step
+        self.position = 0
+        self.historic_position = np.zeros((self.window_size, 1))
+        self.done = False
+        self.orders = []
+        self.historic_action = np.full((self.window_size, 1), 0)
+        self.wallet = 0
 
-        state = self.data[self.initial_step-self.window_size:self.initial_step].copy()
-        #state = np.concatenate((state,self.historic_wallet),axis = 1)
-        self.state_size = np.shape(state[:,1:])
+    def reset(self, initial_step: int = -1):
+        self.initial_step = rd.randint(self.window_size, len(self.data) - (self.n * self.episode_size + 1)) if initial_step < 0 else initial_step
+        self.current_step = self.initial_step
+        self.done = False
+        self.wallet = 0
+        self.orders = []
+        self.position = 0
+        self.historic_position = np.zeros((self.window_size, 1))
+        self.historic_action = np.full((self.window_size, 1), 0)
+        self.historic_wallet = np.full((self.window_size, 1), self.wallet)
 
-        return state[:,1:]
+        state = self.data[self.initial_step - self.window_size:self.initial_step].copy()
+        self.state_size = np.shape(state[:, 1:])
+        return state[:, 1:]
 
     def hold(self):
         pass
 
-    def step(self,action):
+    def step(self, action):
         if self.done:
-            raise ValueError(
-                "La partie est déjà terminée. Appelez la méthode reset() pour recommencer."
-            )
+            raise ValueError("The episode is already done. Call reset() to start a new episode.")
 
         if self.position == 0:
-          if action == 0:
-            self.orders.append(Order())
-            self.orders[-1].place_order(self.current_step, 1)
-            self.position = self.orders[-1].order_type
-          elif action == 1:
-            self.orders.append(Order())
-            self.orders[-1].place_order(self.current_step, -1)
-            self.position = self.orders[-1].order_type
-          else :
-           self.hold()
-
-        if self.position == 1:
-          if action == 0:
-            self.hold()
-          elif action == 1:
-            self.orders[-1].close_order(self.current_step)
-            self.orders.append(Order())
-            self.orders[-1].place_order(self.current_step, -1)
-            self.position = self.orders[-1].order_type
-          else :
-            self.orders[-1].close_order(self.current_step)
-            self.position = 0
-
-        if self.position == -1:
-          if action == 1:
-            self.hold()
-          elif action == 0:
-            self.orders[-1].close_order(self.current_step)
-            self.orders.append(Order())
-            self.reward=0
-            self.orders[-1].place_order(self.current_step, 1)
-            self.position = self.orders[-1].order_type
-          else :
-            self.orders[-1].close_order(self.current_step)
-            self.position = 0
+            self._open_position(action)
+        elif self.position == 1:
+            self._handle_long_position(action)
+        elif self.position == -1:
+            self._handle_short_position(action)
 
         self.current_step += 1
-        
-        current_price = self.data[self.current_step][0]
-        previous_price = self.data[self.current_step-1][0]
-        self.wallet += self.position*(current_price-previous_price)/0.001
-        #self.historic_position = np.concatenate((self.historic_position, np.array([[self.position]])),axis = 0)
-        self.historic_wallet= np.concatenate((self.historic_wallet, np.array([[self.wallet]])),axis = 0)
-        #self.historic_action = np.concatenate((self.historic_action, np.array([[action]])),axis = 0)
-        
-        E = self.data[self.current_step-self.window_size:self.current_step].copy()
-        #U = self.historic_position[self.current_step-self.initial_step:].copy()
-        #I = self.historic_action[self.current_step-self.window_size:].copy()
-        #R = self.historic_wallet[self.current_step-self.window_size:].copy()
+        self._update_wallet()
 
-        #print(np.shape(E),np.shape(U),np.shape(I))
-
-        next_state = E #np.concatenate((E,I,R) , axis = 1)
+        next_state = self.data[self.current_step - self.window_size:self.current_step].copy()
         reward = self.calculate_reward()
 
-        if self.current_step-self.initial_step >= self.episode_size :
+        if self.current_step - self.initial_step >= self.episode_size:
             self.done = True
-            if len(self.orders) != 0 and self.orders[-1].end_date == 0 :
+            if self.orders and self.orders[-1].end_date == 0:
                 self.orders[-1].close_order(self.current_step)
                 self.position = 0
-        return next_state[:,1:], reward, self.done, action,{}
+
+        return next_state[:, 1:], reward, self.done, action, {}
+
+    def _open_position(self, action):
+        if action == 0:
+            self.orders.append(Order())
+            self.orders[-1].place_order(self.current_step, 1)
+            self.position = 1
+        elif action == 1:
+            self.orders.append(Order())
+            self.orders[-1].place_order(self.current_step, -1)
+            self.position = -1
+        else:
+            self.hold()
+
+    def _handle_long_position(self, action):
+        if action == 0:
+            self.hold()
+        elif action == 1:
+            self.orders[-1].close_order(self.current_step)
+            self.orders.append(Order())
+            self.orders[-1].place_order(self.current_step, -1)
+            self.position = -1
+        else:
+            self.orders[-1].close_order(self.current_step)
+            self.position = 0
+
+    def _handle_short_position(self, action):
+        if action == 1:
+            self.hold()
+        elif action == 0:
+            self.orders[-1].close_order(self.current_step)
+            self.orders.append(Order())
+            self.orders[-1].place_order(self.current_step, 1)
+            self.position = 1
+        else:
+            self.orders[-1].close_order(self.current_step)
+            self.position = 0
+
+    def _update_wallet(self):
+        current_price = self.data[self.current_step][0]
+        previous_price = self.data[self.current_step - 1][0]
+        self.wallet += self.position * (current_price - previous_price) / 0.001
+        self.historic_wallet = np.concatenate((self.historic_wallet, np.array([[self.wallet]])), axis=0)
+
     def calculate_reward(self):
-       return self.historic_wallet[-1][0]-self.historic_wallet[-2][0]
-
-
-        
+        return self.historic_wallet[-1][0] - self.historic_wallet[-2][0]
